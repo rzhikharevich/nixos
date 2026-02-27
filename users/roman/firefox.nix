bdf1e4a84f7927e3cca005745d733e515790cc90
Roman Zhikharevich <rzhikharevich@gmail.com>
2026-02-27 08:26:07 +0000

    Use DuckDuckGo + privacy settings

--- users/roman/firefox.nix
+++ users/roman/firefox.nix
@@ -2,6 +2,7 @@
 {
   programs.firefox = {
     enable = true;
+
     profiles.default.settings = {
       "browser.newtabpage.activity-stream.showSponsored" = false;
       "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
@@ -13,11 +14,22 @@
 
       "dom.min_background_timeout_value" = 10000;
       "beacon.enabled" = false;
+      "privacy.resistFingerprinting" = true;
     };
+
+    profiles.default.search = {
+      force = true;
+      default = "ddg";
+      privateDefault = "ddg";
+    };
+
     policies = {
+      DisableTelemetry = true;
+
       ExtensionSettings."uBlock0@raymondhill.net" = {
         install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
         installation_mode = "force_installed";
+        private_browsing = true;
       };
       "3rdparty".Extensions."uBlock0@raymondhill.net".toOverwrite.filterLists = [
         "user-filters"
