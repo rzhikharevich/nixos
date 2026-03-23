6f6c9a6a84c219f81c9cd030bbeecba4615056d1
Roman Zhikharevich <rzhikharevich@gmail.com>
2026-03-23 11:40:25 +0000

    Comment out isolcpus

--- hosts/nixform/default.nix
+++ hosts/nixform/default.nix
@@ -26,8 +26,8 @@
       "irqaffinity=0,1,8,9"
 
       # Micro-benchmarking:
-      "isolcpus=4,12"
-      "nohz_full=4,12"
+      # "isolcpus=4,12"
+      # "nohz_full=4,12"
 
       # threadirqs is interesting since it would presumably bring IRQs under lavd's control but
       # it's probably more trouble (overhead) than it's worth.
