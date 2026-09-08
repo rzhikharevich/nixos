3038cbc5d7c5113dd7082f22a67983815287b74a
Roman Zhikharevich <rzhikharevich@gmail.com>
2026-09-08 05:44:57 +0100

    lagrange gaming tweaks

--- users/roman/hosts/lagrange/default.nix
+++ users/roman/hosts/lagrange/default.nix
@@ -8,5 +8,13 @@
   programs.mangohud = {
     enable = true;
     enableSessionWide = true;
+    settings.fps_limit = 116;
   };
+
+  programs.niri.settings.window-rules = [
+    {
+      matches = [ { app-id = "^steam_app_[0-9]+$"; } ];
+      variable-refresh-rate = true;
+    }
+  ];
 }
